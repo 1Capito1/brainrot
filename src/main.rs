b0e116f4670dfe7b90c0f1ce071e8d7ab54183b0
capito <seth.l.tootell@gmail.com>
2025-01-14 00:56:10 +1100

    it's 1am ok

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use anyhow::Result;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about=None)]
+struct Args {
+    file_name: String,
+    #[arg(short, long, default_value_t = 1)]
+    count: u8
+}
+fn run_file(_path: &String) -> Result<()> {
+    Ok(())
+}
+fn _run_prompt(_path: &String) -> Result<()> {
+    Ok(())
+}
+fn main() -> Result<()> {
+    let args = Args::parse();
+
+    if args.count > 1 {
+        println!("too many args");
+        return Ok(())
+    }
+    else if args.count == 1 {
+        run_file(&args.file_name)?;
+    }
+    else {
+        todo!();
+    }
+    println!("{:?}", args);
+
+    Ok(())
 }
